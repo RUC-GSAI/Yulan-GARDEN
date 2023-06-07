c815e9e0212e173fdf00aa81bf45204c71309b95
emanual20 <emanual20.sun@gmail.com>
2023-06-07 20:35:27 +0800

    update cleaner, filter and settings

--- utils/cleaner.py
+++ utils/cleaner.py
@@ -1,3 +1,67 @@
+from utils.settings import *
+import re
+
 class Cleaner:
-    def __init__(self):
-        pass
+    def __init__(self, setting: Settings=None) -> None:
+        self.clean_setting = {}
+        self.load_settings(setting=setting)
+
+    def load_settings(self, setting: Settings) -> None:
+        self.clean_setting = setting['clean_paras']
+        self.rm_crawlerchars = self.clean_setting['rm_crawlerchars']
+        self.sub_newline = self.clean_setting['sub_newline']
+        self.rm_re_rules = self.clean_setting['rm_re_rules']['re_list'] if self.clean_setting['rm_re_rules']['use'] else []
+        self.sub_re_rules = self.clean_setting['sub_re_rules']['re_dict'] if self.clean_setting['sub_re_rules']['use'] else dict()
+        self.rm_str_rules = self.clean_setting['rm_str_rules']['str_list'] if self.clean_setting['rm_str_rules']['use'] else []
+
+    def clean_single_text(self, text: str) -> str:
+        if self.rm_crawlerchars['use']:
+            text = self._rm_crawlerchars(text)
+        if self.sub_newline['use']:
+            text = self._sub_newline(text)
+        if len(self.rm_re_rules) > 0:
+            for rm_re in self.rm_re_rules:
+                text = self._rm_re(text, rm_re)
+        if len(self.sub_re_rules) > 0:
+            for sub_re, repl_text in self.sub_re_rules:
+                text = self._sub_re(text, sub_re, repl_text)
+        if len(self.rm_str_rules) > 0:
+            for rm_str in self.rm_str_rules:
+                text = self._rm_text(text, rm_str)
+        return text    
+
+    def _rm_re(self, text: str, re_text) -> str:
+        '''
+        remove the 're_text' in the text
+        '''
+        return re.sub(pattern=re_text, repl='', string=text)
+
+    def _rm_text(self, text: str, rm_str: str) -> str:
+        '''
+        remove the 'rm_str' in the text
+        '''
+        return text.replace(rm_str, '')
+    
+    def _sub_re(self, text: str, re_text, repl_text: str) -> str:
+        '''
+        're_text' is substituted with 'repl_text'
+        '''
+        return re.sub(pattern=re_text, repl=repl_text, string=text)
+
+    def _rm_crawlerchars(self, text: str) -> str:
+        '''
+        remove the unused patterns in the text
+        '''
+        patterns = ['\xa0', '\u3000', '&nbsp']
+        for pattern in patterns:
+            text = self._rm_text(text, pattern)
+        return text
+    
+    def _sub_newline(self, text: str) -> str:
+        '''
+        remove consecutive newlines in the text
+        '''
+        patterns = [r'[ |\t]+\n', '\\n']
+        for pattern in patterns:
+            text = self._rm_re(text, pattern)
+        return text
--- utils/filter.py
+++ utils/filter.py
@@ -1,3 +1,84 @@
+from utils.settings import *
+import re
+
 class Filter:
-    def __init__(self):
-        pass
+    def __init__(self, setting: Settings=None) -> None:
+        self.filter_setting = {}
+        self.load_settings(setting=setting)
+
+    def load_settings(self, setting: Settings) -> None:
+        self.filter_setting = setting['filter_paras']
+        self.fil_short_texts = self.filter_setting['self.filter_setting']
+        self.fil_non_ch = self.filter_setting['fil_non_ch']
+        self.fil_copyright = self.filter_setting['fil_copyright']
+        self.fil_short_lines = self.filter_setting['fil_short_lines']
+    
+
+    def filter_single_text(self, text: str) -> bool:
+        '''
+        if single text should be filtered (i.e. scattered),
+        return True 
+        '''       
+        if self.fil_short_texts['use']:
+            if self._fil_short_texts(text, self.fil_short_texts['param']):
+                return True
+        if self.fil_non_ch['use']:
+            if self._fil_non_ch(text, self.fil_non_ch['param']):
+                return True
+        
+        if self.fil_copyright['use']:
+            for word in self.fil_copyright['ch_list']:
+                if self._fil_copyright_ch(text, word):
+                    return True
+            for word in self.fil_copyright['en_list']:
+                if self._fil_copyright_en(text, word):
+                    return True
+        
+        if self.fil_short_lines['use']:
+            if self._fil_short_lines(text, self.fil_short_lines['param']):
+                return True
+        
+        return False
+
+    
+    def _fil_short_texts(self, text: str, min_len: float) -> bool:
+        '''
+        if the length of text smaller than 'min_len', 
+        return True (filter this text)
+        '''
+        return len(text) < min_len
+    
+    def _fil_non_ch(self, text: str, non_ch: float) -> bool:
+        '''
+        if the ratio of non Chinese characters more than 'non_ch', 
+        return True (filter this text)
+        '''
+        find = re.findall(r'[^\u4e00-\u9fa5]|[a-zA-Z0-9]', text)                
+        return len(find) / len(text) >= non_ch
+    
+    def _fil_copyright_ch(self, text: str, word: str) -> bool:      
+        '''
+        if the Chinese 'word' exists in text, 
+        return True (filter this text)
+        '''     
+        return re.search(word, text)
+    
+    def _fil_copyright_en(self, text: str, word: str) -> bool:      
+        '''
+        if the English 'word' exists in text, (case-insensitive)
+        return True (filter this text)
+        '''      
+        return re.search(word, text, re.I)
+
+    def _fil_short_lines(self, text: str, threshold: float) -> bool:
+        '''
+        if the ratio of short line (less than 3 words) more than 'threshold',
+        return True (filter this text)
+        '''   
+        splits = text.split('\n')
+        res = sum([1 for each in splits if (len(re.findall(r'[\u4e00-\u9fa5]', each)) <= 3)]) / len(splits)
+        return res > threshold
+        
+
+
+        
